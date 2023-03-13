b91a684d0ad02609c9306d6f1cef66e8b63656a7
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-03-13 18:24:40 -0300

    chore: altering switch/route syntax

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Album from './components/Album';
 import Favorites from './components/Favorites';
 import Login from './components/Login';
@@ -9,16 +9,49 @@ import ProfileEdit from './components/ProfileEdit';
 import Search from './components/Search';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loginEnabled: true,
+      loginNameInput: '',
+    };
+  }
+
+  enableLoginButton = () => {
+    const stringMinLimit = 3;
+    const { loginNameInput } = this.state;
+    if (loginNameInput.length >= stringMinLimit) {
+      this.setState({ loginEnabled: false });
+    } else {
+      this.setState({ loginEnabled: true });
+    }
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, this.enableLoginButton);
+  };
+
   render() {
+    const { loginEnabled, loginNameInput } = this.state;
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/search" component={ Search } />
-        <Route exact path="/album/:id" component={ Album } />
-        <Route exact path="/favorites" component={ Favorites } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/profile/edit" component={ ProfileEdit } />
-        <Route path="" component={ NotFound } />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={ Login }
+            loginEnabled={ loginEnabled }
+            loginNameInput={ loginNameInput }
+            handleChange={ this.handleChange }
+          />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,37 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends Component {
   render() {
+    const { loginEnabled, loginNameInput, handleChange } = this.props;
     return (
       <div data-testid="page-login">
-        <p>Login</p>
+        <h1>Login</h1>
+        <label htmlFor="login-name-input">
+          <input
+            type="text"
+            data-testid="login-name-input"
+            placeholder="qual é o seu nome?"
+            value={ loginNameInput }
+            onChange={ handleChange }
+          />
+        </label>
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          disabled={ loginEnabled }
+        >
+          ENTRAR
+        </button>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  loginEnabled: PropTypes.bool.isRequired,
+  loginNameInput: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
 
 export default Login;

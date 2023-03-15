c62decb8018aebedea46b4da28bbd9859138272d
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-03-14 23:24:13 -0300

    feat: info shown in profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,56 @@
 import { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  state = {
+    isLoading: false,
+    userInfo: {},
+  };
+
+  componentDidMount() {
+    this.fetchProfileInfo();
+  }
+
+  fetchProfileInfo = () => {
+    this.setState({ isLoading: true }, async () => {
+      const userInfo = await getUser();
+      this.setState({
+        isLoading: false,
+        userInfo,
+      });
+      console.log(userInfo);
+    });
+  };
+
   render() {
+    const { isLoading, userInfo } = this.state;
+    const { name, email, image, description } = userInfo;
     return (
       <div data-testid="page-profile">
         <Header />
-        <p>Profile</p>
+        {
+          (isLoading) ? (
+            <Loading />
+          ) : (
+            <div>
+              <img src={ image } alt="Foto do perfil" data-testid="profile-image" />
+              <div>
+                <h4>Nome</h4>
+                <p>{name}</p>
+              </div>
+              <div>
+                <h4>E-mail</h4>
+                <p>{email}</p>
+              </div>
+              <div>
+                <h4>Descrição</h4>
+                <p>{description}</p>
+              </div>
+            </div>
+          )
+        }
       </div>
     );
   }

8505b78e99a5bee843168268729732b1766413f4
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-03-16 18:53:57 -0300

    style: stylizing loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,13 @@
 import { Component } from 'react';
+import '../assets/styles/Loading.css';
+import loadingIcon from '../assets/images/svg/loading.svg';
 
 class Loading extends Component {
   render() {
     return (
-      <div>
-        <p>Carregando...</p>
+      <div className="loading-container">
+        <img src={ loadingIcon } alt="Carregando" />
+        <p className="loading-text">carregando...</p>
       </div>
     );
   }
